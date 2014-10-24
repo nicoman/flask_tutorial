55dfca72a0a863c9163d5f3c5c40a8fd00940352
Marcelo Nicolas Manso <info@nicolasmanso.com.ar>
2014-10-23 23:06:51 -0300

    Add an activate Flask-WTF

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
 
 app = Flask(__name__)
+# Add config file
+app.config.from_object('config')
+
 
 from app import views
--- app/views.py
+++ app/views.py
@@ -2,9 +2,10 @@ from flask import render_template
 from app import app
 
 # Route recorators
+
+
 @app.route('/')
 @app.route('/index')
-
 def index():
     user = {'nickname': 'Miguel'}
     posts = [{'author': {'nickname': 'John'},
