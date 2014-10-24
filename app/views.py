17bf819a555373ca730ddd46f8d54294a5b86604
Marcelo Nicolas Manso <info@nicolasmanso.com.ar>
2014-10-24 08:10:40 -0300

    Start working login form

--- app/views.py
+++ app/views.py
@@ -1,11 +1,12 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from .forms import LoginForm
 
 # Route recorators
 
 
 @app.route('/')
-@app.route('/index')
+@app.route('/index') #default only GET
 def index():
     user = {'nickname': 'Miguel'}
     posts = [{'author': {'nickname': 'John'},
@@ -17,3 +18,11 @@ def index():
                             title='Home',
                             posts=posts,
                             user=user)
+
+
+@app.route('/login', methods=['GET', 'POST']) # Accepts GET and POST request
+def login():
+    form = LoginForm()
+    return render_template('login.html',
+                            title="Sign In",
+                            form=form)
