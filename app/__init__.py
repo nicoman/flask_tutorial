5e866b1f31b64b035fe32ebf44dcdc9dae5a895a
Marcelo Nicolas Manso <info@nicolasmanso.com.ar>
2014-10-29 22:33:57 -0300

    Activate two new login modules

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,12 @@
+import os
+
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+
+from flask.ext.login import LoginManager
+from flask.ext.openid import OpenID
+from config import basedir
+
 app = Flask(__name__)
 
 # Add config file
@@ -7,5 +14,10 @@ app.config.from_object('config')
 
 # Define db object
 db = SQLAlchemy(app)
+
+lm = LoginManager()
+lm.init.app(app)
+# Need a path tmp
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
 
 from app import views, models
