e382369548157b7e9377ec86230536c2dacc0e35
Marcelo Nicolas Manso <nicolasmanso@gmail.com>
2014-11-11 17:54:19 -0300

    Add many-to-many relation

--- app/models.py
+++ app/models.py
@@ -2,6 +2,13 @@ from app import db
 from hashlib import md5
 
 
+# Association table (auxiliar)
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+    )
+
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nickname = db.Column(db.String(64), index=True, unique=True)
@@ -10,6 +17,13 @@ class User(db.Model):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime)
+    # Add many to many relation
+    followed = db.relationship('User',
+                                secondary=followers,
+                                primaryjoin=(followers.c.follower_id == id),
+                                secondaryjoin=(followers.c.followed_id == id),
+                                backref=db.backref('followers', lazy='dynamic'),
+                                lazy='dynamic')
 
     """
     Methods expected by Flask-Login
