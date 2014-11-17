b732bb1c5a6a7806d670e5c125b1c4100382a1b1
Marcelo Nicolas Manso <nicolasmanso@gmail.com>
2014-11-17 17:56:01 -0300

    Add migration

--- db_repository/versions/003_migration.py
+++ db_repository/versions/003_migration.py
@@ -5,6 +5,11 @@ from migrate import *
 from migrate.changeset import schema
 pre_meta = MetaData()
 post_meta = MetaData()
+followers = Table('followers', post_meta,
+    Column('follower_id', Integer),
+    Column('followed_id', Integer),
+)
+
 user = Table('user', post_meta,
     Column('id', Integer, primary_key=True, nullable=False),
     Column('nickname', String(length=64)),
@@ -19,6 +24,7 @@ def upgrade(migrate_engine):
     # migrate_engine to your metadata
     pre_meta.bind = migrate_engine
     post_meta.bind = migrate_engine
+    post_meta.tables['followers'].create()
     post_meta.tables['user'].columns['about_me'].create()
     post_meta.tables['user'].columns['last_seen'].create()
 
@@ -27,5 +33,6 @@ def downgrade(migrate_engine):
     # Operations to reverse the above upgrade go here.
     pre_meta.bind = migrate_engine
     post_meta.bind = migrate_engine
+    post_meta.tables['followers'].drop()
     post_meta.tables['user'].columns['about_me'].drop()
     post_meta.tables['user'].columns['last_seen'].drop()
